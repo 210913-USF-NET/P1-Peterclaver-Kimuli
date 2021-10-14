20366bfbb734bbbcc7cd23c3b2839f2271e9b9b5
PeterclaverKimuli <30950839+PeterclaverKimuli@users.noreply.github.com>
2021-10-14 13:09:42 -0400

    Enabled distributed session memory

--- WebUI/Startup.cs
+++ WebUI/Startup.cs
@@ -44,7 +44,15 @@ namespace WebUI
             //^that's our dependency injection.
             //We're "registering" the dependencies that our controllers need!
 
-            services.AddSession();
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
